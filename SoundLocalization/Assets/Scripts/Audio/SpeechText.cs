38bd6f2271e6de3129a0c491bec1081b9d6183f8
Chris Schipper <schippcj@miamioh.edu>
2016-12-04 16:21:01 -0500

    Created SpeechText file that controls the text that is heard from the microphone

---
+++ SoundLocalization/Assets/Scripts/Audio/SpeechText.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpeechText : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var headPosition = Camera.main.transform.position;
+        var gazeDirection = Camera.main.transform.forward*5;
+
+        transform.position = headPosition + gazeDirection;
+        //transform.position += new Vector3(-1, 0, 0);
+        transform.TransformDirection(gazeDirection);
+    }
+
+    public void updateText(string text)
+    {
+        GetComponent<TextMesh>().text = text;
+    }
+}
